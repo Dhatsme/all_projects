4ff7363f39f408b93484a47ca1c260920de79817
Dhatsme <36080817+Dhatsme@users.noreply.github.com>
2022-02-22 11:37:01 +0530

    Create jelly_bean_pkg.sv

---
+++ uvm/uvm_for_Candy_lovers/jelly_bean_pkg.sv
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// Package: jelly_bean_pkg
+//------------------------------------------------------------------------------
+
+package jelly_bean_pkg;
+import uvm_pkg::*;
+
+`include "transactions.svh"
+
+   //---------------------------------------------------------------------------
+   // Typedef: jelly_bean_sequencer
+   //---------------------------------------------------------------------------
+
+   typedef uvm_sequencer#( jelly_bean_transaction ) jelly_bean_sequencer;
+
+`include "sequences.svh"
+`include "agent.svh"
+`include "env.svh"
+`include "test.svh"
+endpackage: jelly_bean_pkg
